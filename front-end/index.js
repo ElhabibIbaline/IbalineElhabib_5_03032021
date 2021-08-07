f0b3136b1645eda3e8c4441df4201ad409c8a290
elhabib ibaline <h.ibaline@gmail.com>
2021-08-07 17:46:18 +0200

    afficherbtn&donnéesPageProduct

--- front-end/index.js
+++ front-end/index.js
@@ -8,34 +8,42 @@ fetch("http://localhost:3000/api/teddies")
 		// Récupération des produits et affichage des données sur la page html
 		for (let i = 0; i < teddies.length; i++) {
 			// Création de div produit
-			let divcontainer = document.createElement("div");
-			divcontainer.classList.add("article");
-			divcontainer.classList.add("col-lg-5");
-			container.appendChild(divcontainer);
+			let containerArticle = document.createElement("div");
+			containerArticle.classList.add("article");
+			containerArticle.classList.add("col-lg-5");
+			container.appendChild(containerArticle);
 
 			//Ajout de l'image produit
 			let imgTeddy = document.createElement("img");
 			imgTeddy.classList.add("card-img");
 			imgTeddy.setAttribute("src", teddies[i].imageUrl);
-			divcontainer.appendChild(imgTeddy);
+			containerArticle.appendChild(imgTeddy);
 
 			// Ajout du nom du produit h2
 			let h2Teddy = document.createElement("h2");
 			h2Teddy.classList.add("card-title");
 			h2Teddy.innerHTML = teddies[i].name;
-			divcontainer.appendChild(h2Teddy);
+			containerArticle.appendChild(h2Teddy);
 
 			// Ajout de la description du produit
 			let descriptionTeddy = document.createElement("p");
 			descriptionTeddy.classList.add("card-prix");
 			descriptionTeddy.innerHTML = teddies[i].description;
-			divcontainer.appendChild(descriptionTeddy);
+			containerArticle.appendChild(descriptionTeddy);
 
 			// Ajout du prix du produit
 			let prixTeddy = document.createElement("p");
 			prixTeddy.classList.add("card-prix");
 			prixTeddy.innerHTML = teddies[i].price / 100 + " €";
-			divcontainer.appendChild(prixTeddy);
+			containerArticle.appendChild(prixTeddy);
+
+			//création du lien vers le produit
+			let linkTeddy = document.createElement("a");
+			linkTeddy.classList.add("btn");
+			linkTeddy.classList.add("btn__centre");
+			linkTeddy.href = "product.html?id_teddy=" + teddies[i]._id;
+			linkTeddy.innerHTML = " Acheter ce produit";
+			containerArticle.appendChild(linkTeddy);
 		}
 	})
 	.catch((error) => console.log(error));
---
+++ front-end/product.js
@@ -0,0 +1,62 @@
+// Initialiser le container
+let container = document.getElementById("teddie_container");
+let option = document.getElementById("option");
+let container2 = document.getElementById("teddie_container2");
+
+//Récuperer id
+let params = new URLSearchParams(document.location.href.split("?")[1]);
+
+let url = params.get("id_teddy");
+console.log(url);
+
+fetch("http://localhost:3000/api/teddies/" + url)
+	.then((response) => response.json())
+	.then((ourson) => {
+		// Création de la div
+		let divProduct = document.createElement("div");
+		divProduct.classList.add("card");
+		container.appendChild(divProduct);
+
+		//Ajout de l'image
+		let teddyImg = document.createElement("img");
+		teddyImg.classList.add("card-img");
+		teddyImg.setAttribute("src", ourson.imageUrl);
+		divProduct.appendChild(teddyImg);
+		console.log(teddyImg);
+
+		// Ajout du nom produit h2
+		let h2Teddy = document.createElement("h2");
+		h2Teddy.classList.add("card-title");
+		h2Teddy.innerHTML = ourson.name;
+		divProduct.appendChild(h2Teddy);
+
+		// Ajout du prix du produit 1er paragraphe
+		let pTeddy = document.createElement("p");
+		pTeddy.classList.add("card-prix");
+		pTeddy.innerHTML = "Prix : " + " " + ourson.price / 100 + "€";
+		divProduct.appendChild(pTeddy);
+
+		// Ajout de la description du produit en 2eme paragaraphe
+		let descriptionTeddy = document.createElement("p");
+		descriptionTeddy.classList.add("card-description");
+		descriptionTeddy.innerHTML = ourson.description;
+		divProduct.appendChild(descriptionTeddy);
+
+		//Ajout de l'élément option pour le choix des couleurs
+		let firstoption = document.createElement("option");
+		firstoption.setAttribute("disabled", "disabled");
+		firstoption.setAttribute("selected", "true");
+		firstoption.setAttribute("value", 0);
+		firstoption.textContent = "Sélectionner une couleur";
+		option.appendChild(firstoption);
+
+		//boucle for pour la récupération des couleurs 
+		for (let i = 0; i < ourson.colors.length; i++) {
+			let colorsOption = document.createElement("option");
+			option.classList.add("choix");
+			option.appendChild(colorsOption);
+			colorsOption.setAttribute("value", 1);
+			colorsOption.textContent = ourson.colors[i];
+		}
+	})
+	.catch((error) => console.log(error));
